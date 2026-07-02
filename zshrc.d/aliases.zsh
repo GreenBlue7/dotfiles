448d2b1ae580411e99fd100284906f0bee2006b0
youngmook-jung <ymjung1506@gmail.com>
2026-07-02 10:09:47 +0900

    feat: add candidates for future alias addition

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -43,3 +43,41 @@ alias bup='brew update && brew upgrade && brew cleanup && brew doctor && brew li
 
 # cat + pbcopy
 cc() { cat "$1" | pbcopy; echo "Copied: $1"; }
+
+# Alias candidates
+# Navigation
+# alias ..='cd ..'
+# alias ...='cd ../..'
+# alias ~='cd ~'
+# alias -- -='cd -'          # go back to previous directory
+
+# Listing
+# alias ll='ls -alF'
+# alias lt='ls -ltr'         # list by time, newest last
+
+# Safety net (ask before overwriting)
+# alias rm='rm -i'
+# alias cp='cp -i'
+# alias mv='mv -i'
+
+# Git shortcuts
+# alias gs='git status'
+# alias ga='git add .'
+# alias gc='git commit -m'
+# alias gp='git push'
+# alias gl='git log --oneline --graph --decorate'
+# alias gco='git checkout'
+
+# Docker shortcuts
+# alias dps='docker ps'
+# alias dc='docker-compose'
+# alias dcu='docker-compose up -d'
+# alias dcd='docker-compose down'
+
+# Dev shortcuts
+# alias ni='npm install'
+# alias nrd='npm run dev'
+# alias nrb='npm run build'
+
+# Reload shell config after editing
+# alias reload='source ~/.bashrc'
